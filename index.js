ddc525844fa6d55c1712f31031e5b53804ee07d5
Kaz De Jesus <kazdj21@hotmail.com>
2022-09-09 10:28:39 +1000

    Overhauled the user interface again.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const fileReader = new FileReader();
 const fileInputs = document.querySelectorAll(".fileInput");
-const submit = document.querySelector("input[type=submit]");
 const radio = document.querySelectorAll("input[type='radio']");
+const submit = document.querySelector("input[type='submit']");
 
 let fileBuffer1 = undefined;
 let fileBuffer2 = undefined;
 
 fileInputs[0].addEventListener("input", function(event) {
 
-    fileInputText = fileInputs[0].nextElementSibling;
+
+    let fileInputText = fileInputs[0].nextElementSibling;
     fileInputText.innerHTML = "Uploading";
-    fileContents = fileReader.readAsArrayBuffer(fileInputs[0].files[0].slice(0, 10 * 1024 * 1024));
+    fileInputText.classList.remove("equal");
+    fileInputText.classList.remove("inequal");
+    fileContents = fileReader.readAsBinaryString(fileInputs[0].files[0].slice(0, 10 * 1024 * 1024));
     fileReader.onload = function (e) {
 
         fileBuffer1 = fileReader.result;
@@ -22,9 +25,11 @@ fileInputs[0].addEventListener("input", function(event) {
 
 fileInputs[1].addEventListener("input", function(event) {
 
-    fileInputText = fileInputs[1].nextElementSibling;
+    let fileInputText = fileInputs[1].nextElementSibling;
     fileInputText.innerHTML = "Uploading";
-    fileContents = fileReader.readAsArrayBuffer(fileInputs[1].files[0].slice(0, 10 * 1024 * 1024));
+    fileInputText.classList.remove("equal");
+    fileInputText.classList.remove("inequal");
+    fileContents = fileReader.readAsBinaryString(fileInputs[1].files[0].slice(0, 10 * 1024 * 1024));
 
     fileReader.onload = function (e) {
 
@@ -38,71 +43,73 @@ fileInputs[1].addEventListener("input", function(event) {
 submit.addEventListener("click", function(event) {
 
     event.preventDefault();
-    const p1 = fileInputs[0].nextElementSibling.nextElementSibling;
-    const p2 = fileInputs[1].nextElementSibling.nextElementSibling;
 
-    p1.classList.value = ""
-    p2.classList.value = ""
+    let fileInputText1 = fileInputs[0].nextElementSibling;
+    let fileInputText2 = fileInputs[1].nextElementSibling;
 
-    if (fileInputs[0] === undefined | fileInputs[1] === undefined) {
+    for (let i = 0; i < radio.length; i++) {
 
-        console.log("Upload some files please.");
+        if (radio[i].checked) {
 
-    } else {
+            switch (radio[i].id) {
 
-        for (let i = 0; i < radio.length; i++) {
+                case "md5":
 
-            if (radio[i].checked) {
+                    file1 = objectHash.MD5(fileBuffer1);
+                    file2 = objectHash.MD5(fileBuffer2);
 
-                switch (radio[i].id) {
+                    fileInputText1 = fileInputs[0].nextElementSibling;
+                    fileInputText1.classList.remove("equal");
+                    fileInputText1.classList.remove("inequal");
+                    fileInputText1.innerHTML = file1;
 
-                    case "md5":
-                        file1 = objectHash.MD5(fileBuffer1);
-                        file2 = objectHash.MD5(fileBuffer2);
-                        p1.innerHTML = file1;
-                        p2.innerHTML = file2;
+                    fileInputText2 = fileInputs[1].nextElementSibling;
+                    fileInputText2.innerHTML = file2;
+                    fileInputText2.classList.remove("equal");
+                    fileInputText2.classList.remove("inequal");
 
-                        if (file1 === file2) {
+                    if (file1 === file2) {
 
-                            p1.classList.add("equal")
-                            p2.classList.add("equal")
+                        fileInputText1.classList.add("equal");
+                        fileInputText2.classList.add("equal");
 
-                        } else {
+                    } else {
 
-                            p1.classList.add("inequal");
-                            p2.classList.add("inequal");
+                        fileInputText1.classList.add("inequal");
+                        fileInputText2.classList.add("inequal");
 
-                        }
-
-
-                        console.log(file1, file2, file1 === file2);
-                        break;
-                    case "sha1":
-                        file1 = objectHash.sha1(fileBuffer1);
-                        file2 = objectHash.sha1(fileBuffer2);
-                        p1.innerHTML = file1;
-                        p2.innerHTML = file2;
-
-                        if (file1 === file2) {
-
-                            p1.classList.add("equal")
-                            p2.classList.add("equal")
-
-                        } else {
-
-                            p1.classList.add("inequal");
-                            p2.classList.add("inequal");
-
-                        }
-
-
-                        console.log(file1, file2, file1 === file2);
-                        break;
+                    }
+                    break;
+                
+                case "sha1":
+                    file1 = objectHash.sha1(fileBuffer1);
+                    file2 = objectHash.sha1(fileBuffer2);
+    
+                    fileInputText1 = fileInputs[0].nextElementSibling;
+                    fileInputText1.classList.remove("equal");
+                    fileInputText1.classList.remove("inequal");
+                    fileInputText1.innerHTML = file1;
+    
+                    fileInputText2 = fileInputs[1].nextElementSibling;
+                    fileInputText2.innerHTML = file2;
+                    fileInputText2.classList.remove("equal");
+                    fileInputText2.classList.remove("inequal");
+    
+                    if (file1 === file2) {
+    
+                        fileInputText1.classList.add("equal");
+                        fileInputText2.classList.add("equal");
+    
+                    } else {
+    
+                        fileInputText1.classList.add("inequal");
+                        fileInputText2.classList.add("inequal");
+    
+                    }
+                    break;
+                    
                     default:
-                        console.log("none was selected");
                         break;
-
-                }
 
             }
 
